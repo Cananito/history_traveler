752f4de92da261ad41d0b62bf5dbf7f8ec6456df
Rogelio Gudino <cananito@gmail.com>
2023-10-30 22:54:02 -0700

    Removes leading underscore from global variables.

--- js/main.js
+++ js/main.js
@@ -1,11 +1,11 @@
 // UI elements.
 
-let _backButton;
-let _forwardButton;
-let _pushButton;
-let _popToRootButton;
-let _historyIndexInfoSpan;
-let _historyCountInfoSpan;
+let backButton;
+let forwardButton;
+let pushButton;
+let popToRootButton;
+let historyIndexInfoSpan;
+let historyCountInfoSpan;
 
 
 // Event handling.
@@ -50,60 +50,60 @@ function updateUI() {
       (history.length - currentState.firstKnownHistoryLength) + 1;
 
   if (historyIndex < 1) {
-    _backButton.disabled = true;
-    _popToRootButton.disabled = true;
+    backButton.disabled = true;
+    popToRootButton.disabled = true;
   } else {
-    _backButton.disabled = false;
-    _popToRootButton.disabled = false;
+    backButton.disabled = false;
+    popToRootButton.disabled = false;
   }
   if (historyIndex == historyCount - 1) {
-    _forwardButton.disabled = true;
+    forwardButton.disabled = true;
   } else {
-    _forwardButton.disabled = false;
+    forwardButton.disabled = false;
   }
-  _historyIndexInfoSpan.innerHTML = "historyIndex: " + historyIndex;
-  _historyCountInfoSpan.innerHTML = "historyCount: " + historyCount;
+  historyIndexInfoSpan.innerHTML = "historyIndex: " + historyIndex;
+  historyCountInfoSpan.innerHTML = "historyCount: " + historyCount;
 };
 
 function initializeUI() {
   const bodyContentDiv = document.createElement("div");
 
   // back button
-  _backButton = document.createElement("button");
-  _backButton.innerHTML = "Back";
-  _backButton.addEventListener("click", handleBackButtonClick);
-  bodyContentDiv.appendChild(_backButton);
+  backButton = document.createElement("button");
+  backButton.innerHTML = "Back";
+  backButton.addEventListener("click", handleBackButtonClick);
+  bodyContentDiv.appendChild(backButton);
 
   // forward button
-  _forwardButton = document.createElement("button");
-  _forwardButton.innerHTML = "Forward";
-  _forwardButton.addEventListener("click", handleForwardButtonClick);
-  bodyContentDiv.appendChild(_forwardButton);
+  forwardButton = document.createElement("button");
+  forwardButton.innerHTML = "Forward";
+  forwardButton.addEventListener("click", handleForwardButtonClick);
+  bodyContentDiv.appendChild(forwardButton);
 
   // push button
-  _pushButton = document.createElement("button");
-  _pushButton.innerHTML = "Push +1";
-  _pushButton.addEventListener("click", handlePushButtonClick);
-  bodyContentDiv.appendChild(_pushButton);
+  pushButton = document.createElement("button");
+  pushButton.innerHTML = "Push +1";
+  pushButton.addEventListener("click", handlePushButtonClick);
+  bodyContentDiv.appendChild(pushButton);
 
   // pop button (does same as back)
-  _popToRootButton = document.createElement("button");
-  _popToRootButton.innerHTML = "Pop to root";
-  _popToRootButton.addEventListener("click", handlePopToRootButtonClick);
-  bodyContentDiv.appendChild(_popToRootButton);
+  popToRootButton = document.createElement("button");
+  popToRootButton.innerHTML = "Pop to root";
+  popToRootButton.addEventListener("click", handlePopToRootButtonClick);
+  bodyContentDiv.appendChild(popToRootButton);
 
   // line breaks
   bodyContentDiv.appendChild(document.createElement("br"));
   bodyContentDiv.appendChild(document.createElement("br"));
 
   // history length info span
-  _historyIndexInfoSpan = document.createElement("span");
-  bodyContentDiv.appendChild(_historyIndexInfoSpan);
+  historyIndexInfoSpan = document.createElement("span");
+  bodyContentDiv.appendChild(historyIndexInfoSpan);
   bodyContentDiv.appendChild(document.createElement("br"));
 
   // history state info span
-  _historyCountInfoSpan = document.createElement("span");
-  bodyContentDiv.appendChild(_historyCountInfoSpan);
+  historyCountInfoSpan = document.createElement("span");
+  bodyContentDiv.appendChild(historyCountInfoSpan);
   bodyContentDiv.appendChild(document.createElement("br"));
 
   const body = document.body;
